10f8a4b6a0b4f5c0eacbd8ec34bf453355dec19c
Sebastian Martin <sebastian.martin@satellytes.com>
2024-05-24 15:53:44 +0200

    feat: introduce region types

--- generators/urban_areas.py
+++ generators/urban_areas.py
@@ -1,36 +1,119 @@
 import numpy as np
 import random
 
+def select_regional_type():
+    regions = {
+        "rural": {
+            "metropolis": {"min": 0.00, "max": 0.10},
+            "city": {"min": 0.10, "max": 0.15},
+            "town": {"min": 0.15, "max": 0.20}
+        },
+        "urbanized": {
+            "metropolis": {"min": 0.10, "max": 0.15},
+            "city": {"min": 0.20, "max": 0.30},
+            "town": {"min": 0.30, "max": 0.35}
+        },
+        "metropolized": {
+            "metropolis": {"min": 0.35, "max": 0.52},
+            "city": {"min": 0.30, "max": 0.48},
+            "town": {"min": 0.40, "max": 0.55}
+        }
+    }
+    
+    # Select a random regional type from the available types
+    selected_type = random.choice(list(regions.keys()))
+    return regions[selected_type]
+    
+
 def distribute_population(total_population: int):
-    # metropolises = 0
-    cities = 0
-    towns = 0
-    villages = 0
+    regional_type = select_regional_type()
+    metropolitan_population = total_population * random.uniform(regional_type["metropolis"]["min"], regional_type["metropolis"]["max"])
+    metropolis_base_population = 1_500_000
     
-    metropolitan_population = total_population * random.uniform(0.20, 0.30)
-    cities_population = total_population * 0.08
-    towns_population = total_population * 0.27
-    villages_population = total_population * 0.63
-    
-    no_of_new_metropolises = round(metropolitan_population / 1_500_000)
+    no_of_new_metropolises = round(metropolitan_population / metropolis_base_population)
     
     metropolises = []
     left_to_distribute = total_population
     metropolitan_population_absolute = 0
 
     if no_of_new_metropolises > 0:
-        new_metropolises = np.random.normal(1_500_000, 350_000, no_of_new_metropolises)
+        new_metropolises = np.random.normal(metropolis_base_population, 350_000, no_of_new_metropolises)
         
         for new_metropolis in new_metropolises:
             metropolises.append(round(new_metropolis))
             left_to_distribute -= round(new_metropolis)
             metropolitan_population_absolute += round(new_metropolis)
         
-        perc = metropolitan_population_absolute / total_population
+        met_perc = metropolitan_population_absolute / total_population
     else:
-        perc = 0
+        met_perc = 0
     
-    print(f"Metropolises: {len(metropolises)} with a total of {metropolitan_population_absolute} people ( {perc:2f} %)")
+    print(f"Metropolises: {len(metropolises)} with a total of {metropolitan_population_absolute} people ( {met_perc:2f} %)")
+    
+    cityish_population = left_to_distribute * random.uniform(regional_type["city"]["min"], regional_type["city"]["max"])
+    cityish_base_population = 200_000
+    no_of_new_cities = round(cityish_population / cityish_base_population)
+    
+    cities = []
+    cityish_population_absolute = 0
+    
+    if no_of_new_cities > 0:
+        new_cities = np.random.normal(cityish_base_population, 50_000, no_of_new_cities)
+        
+        for new_city in new_cities:
+            cities.append(round(new_city))
+            left_to_distribute -= round(new_city)
+            cityish_population_absolute += round(new_city)
+        
+        cit_perc = cityish_population_absolute / total_population
+    else:
+        cit_perc = 0
+        
+    print(f"Cities: {len(cities)} with a total of {cityish_population_absolute} people ( {cit_perc:2f} %)")
+    
+    townish_population = left_to_distribute * random.uniform(regional_type["town"]["min"], regional_type["town"]["max"])
+    town_base_population = 20_000
+    no_of_new_towns = round(townish_population / town_base_population)
+    
+    towns = []
+    townish_population_absolute = 0
+    
+    if no_of_new_towns > 0:
+        new_towns = np.random.normal(town_base_population, 5_000, no_of_new_towns)
+        
+        for new_town in new_towns:
+            towns.append(round(new_town))
+            left_to_distribute -= round(new_town)
+            townish_population_absolute += round(new_town)
+        
+        town_perc = townish_population_absolute / total_population
+    else:
+        town_perc = 0
+        
+    print(f"Towns: {len(towns)} with a total of {townish_population_absolute} people ( {town_perc:2f} %)")
+
+    villages_population = left_to_distribute
+    village_base_population = 2000
+    no_of_new_villages = round(villages_population / village_base_population)
+    
+    villages = []
+    village_population_absolute = 0
+    
+    if no_of_new_villages > 0:
+        new_villages = np.random.normal(village_base_population, 500, no_of_new_villages)
+        
+        for new_village in new_villages:
+            villages.append(round(new_village))
+            left_to_distribute -= round(new_village)
+            village_population_absolute += round(new_village)
+        
+        vil_perc = village_population_absolute / total_population
+    else:
+        vil_perc = 0
+        
+    print(f"Villages: {len(villages)} with a total of {village_population_absolute} people ( {vil_perc:2f} %)")
+    
+    print(f"{met_perc + cit_perc + town_perc + vil_perc}")
     return ""
    
     
